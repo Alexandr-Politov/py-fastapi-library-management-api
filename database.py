418fbe913763a8c7a8d951468deee538c79a0778
Alexandr Politov <apolitov83@gmail.com>
2024-10-26 13:14:09 +0300

    db engine created in database.py

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+SQLALCHEMY_DATABASE_URL = "sqlite:///./library_manager.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
