acaf1ae599fc6b10ff5d7d62a62850764589dee6
Alexandr Politov <apolitov83@gmail.com>
2024-10-26 15:07:46 +0300

    endpoint for authors-list created

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+import crud, database, schemas
 
 app = FastAPI()
 
 
+def get_db_session() -> Session:
+    db_session = database.SessionLocal()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
+
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to FastAPI library manager"}
+
+
+@app.get("/authors/", response_model=list[schemas.AuthorInfo])
+def read_authors(db_session: Session = Depends(get_db_session)):
+    return crud.get_all_authors(db_session)
